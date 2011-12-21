7c106c127cf9a0089335e19f53063c20bb4d9933
Koen Ongena <gijoke@gmail.com>
2011-12-21 22:06:25 +0100

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: koen
+ * Date: 21/12/11
+ * Time: 22:04
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
